4611ac6a131ab33f679f004d40c22d7f24911045
xiaotantanya <2634918190@qq.com>
2024-05-18 12:43:53 +0800

    add requirements.txt

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ extensions = ['recommonmark', 'sphinx_markdown_tables']
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'en'
+language = 'zh_CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
